75db78cd08456919a4377e6e83e1e8bd8ba8595a
Julio <juliovel86@gmail.com>
2025-06-20 10:10:09 -0600

    fix: include _redirects in dist with vite-plugin-static-copy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.' // Esto lo coloca directamente en la raíz de /dist
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
